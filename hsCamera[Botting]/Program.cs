7ba855763da26cb3e7aa32b5486b77dafc947819
drunkenninja <drunken_ninja@hotmail.com>
2016-08-29 22:27:32 +0300

    Cam for Botting

--- hsCamera[Botting]/Program.cs
+++ hsCamera[Botting]/Program.cs
@@ -1,5 +1,5 @@
-using System;
-using System.Linq;
+﻿using System;
+using System.Collections.Generic;
 using LeagueSharp;
 using LeagueSharp.Common;
 using SharpDX;
@@ -15,18 +15,27 @@ namespace hsCamera
         }
 
         private static Menu _config;
-
+        /// <summary>
+        /// Enemies stored - Incoming Improvements :O
+        /// </summary>
+        private static Dictionary<int, int> _enemies = new Dictionary<int, int>();
+        /// <summary>
+        /// Amazing OnLoad :jew:
+        /// </summary>
+        /// <param name="args"></param>
         private static void HsCameraOnLoad(EventArgs args)
         {
-            _config = new Menu("hsCamera", "hsCamera", true);
+            _config = new Menu("hsCamera [Botting]", "hsCamera", true);
             {
-                _config.AddItem(new MenuItem("show.enemy.1", "Show Enemy 1 Location").SetValue(new KeyBind(97, KeyBindType.Press)));
+                /*_config.AddItem(new MenuItem("show.enemy.1", "Show Enemy 1 Location").SetValue(new KeyBind(97, KeyBindType.Press)));
                 _config.AddItem(new MenuItem("show.enemy.2", "Show Enemy 2 Location").SetValue(new KeyBind(98, KeyBindType.Press)));
                 _config.AddItem(new MenuItem("show.enemy.3", "Show Enemy 3 Location").SetValue(new KeyBind(99, KeyBindType.Press)));
                 _config.AddItem(new MenuItem("show.enemy.4", "Show Enemy 4 Location").SetValue(new KeyBind(100, KeyBindType.Press)));
                 _config.AddItem(new MenuItem("show.enemy.5", "Show Enemy 5 Location").SetValue(new KeyBind(101, KeyBindType.Press)));
-                _config.AddItem(new MenuItem("semi.dynamic", "Semi-Dynamic Camera?").SetValue(new KeyBind(32, KeyBindType.Press))); //SPACEBAR
+                */
+                _config.AddItem(new MenuItem("semi.dynamic", "Semi-Dynamic Camera?").SetValue(false));
                 _config.AddItem(new MenuItem("follow.dynamic", "Follow-Champion Camera?").SetValue(true));
+                _config.AddItem(new MenuItem("credits", ".:Botting Version of hsCamera:.")).SetFontStyle(System.Drawing.FontStyle.Bold, SharpDX.Color.DeepPink);
                 _config.AddToMainMenu();
             }
             Game.OnUpdate += HsCameraOnUpdate;
@@ -34,7 +43,7 @@ namespace hsCamera
 
         private static void HsCameraOnUpdate(EventArgs args)
         {
-            if (_config.Item("show.enemy.1").GetValue<KeyBind>().Active)
+            /*if (_config.Item("show.enemy.1").GetValue<KeyBind>().Active)
             {
                 var enemylist = HeroManager.Enemies.ToList();
                 if (enemylist[0].IsValid && enemylist[0] != null && enemylist[0].IsChampion())
@@ -78,14 +87,15 @@ namespace hsCamera
                     var position = enemylist[4].Position;
                     Camera.Position = position;
                 }
-            }
+            }*/
 
-            if (_config.Item("semi.dynamic").GetValue<KeyBind>().Active)
+            if (_config.Item("semi.dynamic").GetValue<bool>())
             {
                 SemiDynamic(Game.CursorPos);
             }
 
             if (_config.Item("follow.dynamic").GetValue<bool>())
+            {
                 SemiDynamic(ObjectManager.Player.Position);
             }
         }
